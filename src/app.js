101547294f1abd3c445ad6f8b9771f45509cc2ca
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-29 00:48:10 -0300

    Initial exercise settings

---
+++ src/app.js
@@ -0,0 +1,31 @@
+
+//=== IMPORTAÇÕES ===
+const express = require("express");
+const path = require("path");
+
+
+
+//=== VARIÁVEIS ===
+const app = express();
+const port = 8000;
+
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
+
+
+//=== MIDDLEWARES ===
+app.use(express.json());
+app.use(express.static(path.resolve("src", "public")));
+app.use(express.urlencoded({ extended:false }));
+
+
+//=== ROTAS ===
+
+
+
+//=== SERVIDOR ===
+app.listen(port, ()=>{
+  console.log(`O servidor está rodando na porta:${port}`);
+});
