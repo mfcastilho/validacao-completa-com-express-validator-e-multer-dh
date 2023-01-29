7b6b9385e5cfdbf02efcb723903f02f1ab509b32
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-29 16:14:00 -0300

    Input sanitization

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -24,15 +24,19 @@ const uploadFile = multer({ storage:storage });
 const validations = [
   check("name")
     .notEmpty().withMessage("O campo nome NÃO pode ser enviado vazio!").bail()
-    .isString().withMessage("O campo nome NÃO aceita números!"),
+    .isString().withMessage("O campo nome NÃO aceita números!").bail()
+    .trim(),
 
   check("email")
     .notEmpty().withMessage("O campo email NÃO pode ser enviado vazio!").bail()
+    .trim().bail()
+    .normalizeEmail().bail()
     .isEmail().withMessage("Digite um formato de email correto!"),
 
   check("password")
     .notEmpty().withMessage("O campo senha NÃO pode ser enviado vazio!").bail()
-    .isLength({ min:6 }).withMessage("A senha precisa ter no mínimo 6 caracteres!"),
+    .isLength({ min:6 }).withMessage("A senha precisa ter no mínimo 6 caracteres!").bail()
+    .trim(),
 
   check("avatar").custom((value, {req})=>{
     let file = req.file;
